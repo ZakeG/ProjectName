ad6817783d09b6c35649ecddf905dd2905106e24
JockeNord <38462133+JockeNord@users.noreply.github.com>
2018-11-05 17:04:38 +0100

    Book ready to get messages

---
+++ GruppK_PROJ/Assets/Scripts/MonoBehaviours/Inventory/QuestBookButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestBookButton : MonoBehaviour {
+
+    public GameObject book;
+    private bool bookOpen;
+
+	void Start () {
+        bookOpen = true;
+    }
+    public void ToggleBookOpen()
+    {
+        if (!bookOpen)
+        {
+            OpenBook();
+        }
+        else
+        {
+            CloseBook();
+        }
+    }
+    public void OpenBook()
+    {
+        bookOpen = true;
+        book.SetActive(bookOpen);
+    }
+
+    public void CloseBook()
+    {
+        bookOpen = false;
+        book.SetActive(bookOpen);
+    }
+
+}

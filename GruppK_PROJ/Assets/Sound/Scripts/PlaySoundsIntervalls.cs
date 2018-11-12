9f01d12d172e1b1429217316f83f77a9a4db0453
TediBean <43949187+TediBean@users.noreply.github.com>
2018-11-12 17:39:09 +0100

    push push baby

---
+++ GruppK_PROJ/Assets/Sound/Scripts/PlaySoundsIntervalls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundsIntervalls : MonoBehaviour {
+
+	private AudioSource[] sources; 
+	public AudioClip Footsteps1;
+	public AudioClip Footsteps2;
+
+
+	public float minDelay;
+	public float maxDelay; 
+
+
+	// Use this for initialization
+	void Start () {
+		sources = GetComponents<AudioSource> ();
+		sources[0].clip = Footsteps1;
+		sources[1].clip = Footsteps2;
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!sources [0].isPlaying) {
+
+			float d = Random.Range (minDelay, maxDelay);
+
+			sources [0].PlayDelayed (d);
+
+		}
+		if (!sources [1].isPlaying) {
+
+			float d = Random.Range (minDelay, maxDelay);
+
+			sources [1].PlayDelayed (d);
+
+		}
+	}
+}

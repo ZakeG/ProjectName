28e809640d3430ad70b9d6d109377f336622fb7d
TediBean <43949187+TediBean@users.noreply.github.com>
2018-11-15 11:44:30 +0100

    boo febee

---
+++ GruppK_PROJ/Assets/Sound/Scripts/Intervalscrows.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intervalscrows : MonoBehaviour {
+
+
+    private AudioSource[] sources;
+    public AudioClip CrowCall;
+
+
+    public float minDelay;
+    public float maxDelay;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        sources = GetComponents<AudioSource>();
+        sources[1].clip = CrowCall;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!sources[0].isPlaying)
+        {
+
+            float d = Random.Range(minDelay, maxDelay);
+
+            sources[0].PlayDelayed(d);
+
+
+        }
+    }
+}

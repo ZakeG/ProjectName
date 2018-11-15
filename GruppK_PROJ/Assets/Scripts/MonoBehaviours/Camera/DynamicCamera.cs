a7590714ab749c03e683cf9bd3633af60373780d
JockeNord <38462133+JockeNord@users.noreply.github.com>
2018-11-15 13:50:17 +0100

    Started dynamic camera

---
+++ GruppK_PROJ/Assets/Scripts/MonoBehaviours/Camera/DynamicCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour {
+
+    public GameObject cameraObject1;
+    public GameObject cameraObject2;
+
+    public void MoveCamera()
+    {
+        if (cameraObject1.activeSelf)
+        {
+            cameraObject1.SetActive(false);
+            cameraObject2.SetActive(true);
+        }
+        else
+        {
+
+            cameraObject1.SetActive(true);
+            cameraObject2.SetActive(false);
+        }
+    } 
+}

2257febf8d6ad6cd33f98eb2bcaa30fd590482ab
JockeNord <38462133+JockeNord@users.noreply.github.com>
2018-11-17 23:00:59 +0100

    Navmesh + camera switch functionality

--- GruppK_PROJ/Assets/Scripts/MonoBehaviours/Camera/DynamicCamera.cs
+++ GruppK_PROJ/Assets/Scripts/MonoBehaviours/Camera/DynamicCamera.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class DynamicCamera : MonoBehaviour {
 
-    public GameObject cameraObject1;
-    public GameObject cameraObject2;
+    public GameObject cameraRigObject1;
+    public GameObject cameraRigObject2;
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");   
+        MoveCamera();
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        MoveCamera();
     }
 
 
     private void MoveCamera()
     {
-        if (cameraObject1.activeSelf)
+        if (cameraRigObject1.activeSelf)
         {
-            cameraObject1.SetActive(false);
-            cameraObject2.SetActive(true);
+            cameraRigObject1.transform.GetChild(0).gameObject.SetActive(false);
+            cameraRigObject1.SetActive(false);
+            cameraRigObject2.transform.GetChild(0).gameObject.SetActive(true);
+            cameraRigObject2.SetActive(true);
         }
         else
         {
-
-            cameraObject1.SetActive(true);
-            cameraObject2.SetActive(false);
+            cameraRigObject1.SetActive(true);
+            cameraRigObject1.transform.GetChild(0).gameObject.SetActive(true);
+            cameraRigObject2.SetActive(false);
+            cameraRigObject2.transform.GetChild(0).gameObject.SetActive(false);
+            
         }
     } 
 }

66c12060150cd746da19cb243fe03b810290fe2c
JockeNord <38462133+JockeNord@users.noreply.github.com>
2018-12-13 17:20:39 +0100

    Påbörjat UI Knappar

--- GruppK_PROJ/Assets/Scripts/MonoBehaviours/Inventory/QuestBookButton.cs
+++ GruppK_PROJ/Assets/Scripts/MonoBehaviours/Inventory/QuestBookButton.cs
@@ -7,9 +7,16 @@ public class QuestBookButton : MonoBehaviour {
     public GameObject book;
     public GameObject newIcon;
     public bool bookOpen;
+    public GameObject activeIconGameObject;
+    private SpriteRenderer activeIconSpriteRenderer;
     private SceneController sceneController;
+    private Color blackColor;
+    private Color yellowColor;
 
     void Start () {
+        blackColor = new Color.black;
+        yellowColor = new Color.yellow;
+        activeIconSpriteRenderer = activeIconGameObject.GetComponent<SpriteRenderer>();
         bookOpen = true;
         sceneController = GameObject.Find("SceneController").GetComponent<SceneController>();
         sceneController.BeforeSceneUnload += OpenBook;
@@ -28,6 +35,8 @@ public class QuestBookButton : MonoBehaviour {
     }
     public void OpenBook()
     {
+        activeIconGameObject.SetActive(true);
+        activeIconSpriteRenderer.Color = yellowColor;
         newIcon.SetActive(false);
         bookOpen = true;
         book.SetActive(bookOpen);
@@ -35,6 +44,8 @@ public class QuestBookButton : MonoBehaviour {
 
     public void CloseBook()
     {
+        activeIconGameObject.SetActive(false);
+        activeIconSpriteRenderer.Color = blackColor;
         bookOpen = false;
         book.SetActive(bookOpen);
     }
